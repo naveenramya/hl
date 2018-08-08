cc0a27c08130e0bcc6be844c4f6c86e06832c841
naveenramya <41774158+naveenramya@users.noreply.github.com>
2018-08-08 12:03:34 +0530

    Create hello.7.java

---
+++ hello.7.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner input= new Scanner(System.in);
+       long N= input.nextLong();
+       for(int i=0;i<N;i++) 
+           System.out.println("Hello");
+  }
+}
